9b565970e85b7ed06b9211f804b7137b17e7e7cb
Yousef <yousefmohsen@outlook.dk>
2022-10-06 12:46:29 +0200

    apollo client provider setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+console.log('accessToken',accessToken)
+const client = new ApolloClient({
+  uri: "https://fbi-api.dbc.dk/default/graphql",
+  cache: new InMemoryCache(),
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+
+  );
 }
 
-export default MyApp
+export default MyApp;
